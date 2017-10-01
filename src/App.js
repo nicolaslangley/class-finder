c4e4483cf17b6784576eb1c6fe187e4c5c03ba6c
Nicolas Langley <nicolasqlangley@gmail.com>
2017-10-01 15:26:08 -0700

    Add updateClassList() to App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import ClassList from './ClassList.js';
 import fire from './fire';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {list: []};
+    this.updateClassList = this.updateClassList.bind(this);
+  }
+
+  updateClassList() {
+    var req = new XMLHttpRequest();
+    var reqListener = function () {
+      console.log(this.responseText);
+    };
+    req.addEventListener("load", reqListener);
+    var firebaseProjectID = fire.options.authDomain.split('.')[0];
+    var url = 'https://us-central1-' + firebaseProjectID + '.cloudfunctions.net/auth';
+    req.open('GET', url);
+    req.setRequestHeader('Content-Type', 'text/plain');
+    req.send();
+
+    // Update the class list here and store in this.state
+    this.setState({list: [url, "Test2", "Test3"]});
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +34,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <ClassList list={["Test", "Test2", "Test3"]} />
+        <ClassList list={this.state.list}/>
+        <button onClick={this.updateClassList}>
+        Update class list
+        </button>
+
       </div>
     );
   }
