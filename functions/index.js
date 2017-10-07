94cad7bddd5ca8b6316b90db1588eeb4cb8d4e3d
Nicolas Langley <nicolasqlangley@gmail.com>
2017-10-07 13:04:21 -0700

    Update cloud function to write results to JSON

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,26 @@
 const functions = require('firebase-functions');
 
 var cheerio = require('cheerio');
-var rp = require('request-promise')
+var rp = require('request-promise');
+var jsonfile = require('jsonfile')
 const cors = require('cors')({ origin: true });
+
+var gcloud = require('google-cloud');
+// Enable Storage
+var gcs = gcloud.storage({
+  projectId: 'class-finder-541'
+});
+// Reference an existing bucket.
+var bucket = gcs.bucket('class-finder-541da.appspot.com');
 
 function scrape_pnca(body) {
   var $ = cheerio.load(body);
   var classList = [];
   var classes = $(".classdesc h3").each(function (i, elem) {
-    classList[i] = { name : $(this).text().trim(),
-                     school : "Pacific Northwest College of Art (PNCA)" };
+    classList[i] = {
+      name: $(this).text().trim(),
+      school: "Pacific Northwest College of Art (PNCA)"
+    };
   });
   return classList;
 }
@@ -20,38 +31,46 @@ function scrape_mhcc(body) {
   });
   var classList = [];
   var classes = $("Name").each(function (i, elem) {
-    classList[i] = { name : $(this).text().trim(),
-                     school : "Mt. Hood Community College (MHCC)" };
+    classList[i] = {
+      name: $(this).text().trim(),
+      school: "Mt. Hood Community College (MHCC)"
+    };
   });
   return classList;
 }
 
 function scrape_pcc(body) {
   var $ = cheerio.load(body);
-  var classList = [];  
-  var classes = $(".course-list > dd > a").each(function(i, elem){
-    classList[i] = { name : $(this).text().trim(),
-                     school : "Portland Community College (PCC)" };
+  var classList = [];
+  var classes = $(".course-list > dd > a").each(function (i, elem) {
+    classList[i] = {
+      name: $(this).text().trim(),
+      school: "Portland Community College (PCC)"
+    };
   });
   return classList;
 }
 
 function scrape_ocac(body) {
   var $ = cheerio.load(body);
-  var classList = [];  
-  var classes = $(".course-teaser > .node-title").each(function(i, elem){
-    classList[i] = { name : $(this).text().trim(),
-                     school : "Oregon College of Art and Craft (OCAC)" };
+  var classList = [];
+  var classes = $(".course-teaser > .node-title").each(function (i, elem) {
+    classList[i] = {
+      name: $(this).text().trim(),
+      school: "Oregon College of Art and Craft (OCAC)"
+    };
   });
   return classList;
 }
 
 function scrape_osu(body) {
   var $ = cheerio.load(body);
-  var classList = [];  
-  var classes = $(".field-content").each(function(i, elem){
-    classList[i] = { name : $(this).text().trim(),
-                     school : "Oregon State University (OSU)" };
+  var classList = [];
+  var classes = $(".field-content").each(function (i, elem) {
+    classList[i] = {
+      name: $(this).text().trim(),
+      school: "Oregon State University (OSU)"
+    };
   });
   return classList;
 }
@@ -84,11 +103,22 @@ exports.findClasses = functions.https.onRequest((req, res) => {
                     console.log("\n===============\n" + "Pulling from OCAC: " + ocac_url + "\n===============\n");
                     totalList = totalList.concat(scrape_ocac(body));
                   }).then(function () {
-                   var osu_url = 'https://pace.oregonstate.edu/catalog';
+                    var osu_url = 'https://pace.oregonstate.edu/catalog';
                     rp(osu_url) // OSU request
                       .then(function (body) {
                         console.log("\n===============\n" + "Pulling from OSU: " + osu_url + "\n===============\n");
                         totalList = totalList.concat(scrape_osu(body));
+                        // Write out list to JSON
+                        var filePath = '/tmp/classlist.json'
+                        jsonfile.writeFile(filePath, totalList, function (err) {
+                          console.error(err)
+                        });
+                        // Upload a local file to a new file to be created in your bucket.
+                        bucket.upload(filePath, function (err, file) {
+                          if (!err) {
+                            console.log("Upload successful");
+                          }
+                        });
                         res.status(200).send(totalList);
                       }).catch(function (err) {
                         console.log("We’ve encountered an error: " + err);
