96a00b4053c96c345ce2a1922a089cd69c58b2e0
Nicolas Langley <nicolasqlangley@gmail.com>
2017-10-07 12:07:22 -0700

    Move updating logic to ClassList class

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
 import './App.css';
 import Header from './components/Header/Header';
 import Masthead from './components/Masthead/Masthead';
 import ClassList from './components/ClassList/ClassList';
-import fire from './utils/fire';
 import Footer from './components/Footer/Footer';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {list: []};
-    this.updateClassList = this.updateClassList.bind(this);
-  }
-
-  updateClassList() {
-    var firebaseProjectID = fire.options.authDomain.split('.')[0];
-    var url = 'https://us-central1-' + firebaseProjectID + '.cloudfunctions.net/findClasses';
-    var req = new XMLHttpRequest();
-    req.open('GET', url, true);
-    req.responsetype = 'text';
-    req.onload = function(e) {
-      if (req.readyState === req.DONE) {
-        if (req.status === 200) {
-            console.log(req.response);
-            this.setState({list: JSON.parse(req.response)});
-        }
-      }
-    }.bind(this);
-    req.send();
-  }
-
   render() {
     return (
       <div className="App">
         <Header />
         <Masthead />
-        <ClassList list={this.state.list} />
-        <Button onClick={this.updateClassList}>
-          Update class list
-        </Button>
+        <ClassList />
         <Footer />
       </div>
     );
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react';
 import { ListGroup, Navbar, Nav, MenuItem, NavDropdown } from 'react-bootstrap';
 import './ClassList.css';
 import Listing from './Listing/Listing';
+import fire from '../../utils/fire';
 
 class ClassList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {list: []};
+    this.updateClassList = this.updateClassList.bind(this);
+    this.updateClassList();
+  }
+
+  updateClassList() {
+    var firebaseProjectID = fire.options.authDomain.split('.')[0];
+    var url = 'https://us-central1-' + firebaseProjectID + '.cloudfunctions.net/findClasses';
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.responsetype = 'text';
+    req.onload = function(e) {
+      if (req.readyState === req.DONE) {
+        if (req.status === 200) {
+            console.log(req.response);
+            this.setState({list: JSON.parse(req.response)});
+        }
+      }
+    }.bind(this);
+    req.send();
+  }
+
   render() {
     return (
       <div>
@@ -21,7 +46,7 @@ class ClassList extends Component {
           </Navbar.Collapse>
         </Navbar>
         <ListGroup>
-          {this.props.list.map(function (listValue) {
+          {this.state.list.map(function (listValue) {
             return <Listing key={listValue.name}
                             title={listValue.name}
                             school={listValue.school} />
