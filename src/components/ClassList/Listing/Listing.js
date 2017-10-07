c48592eedd575cd714b1996eb783c728f1357861
Alexis <agregers@uw.edu>
2017-10-07 13:13:01 -0700

    update listing style and fonts

--- src/components/ClassList/Listing/Listing.js
+++ src/components/ClassList/Listing/Listing.js
@@ -9,13 +9,14 @@ class Listing extends Component {
         <div className="listingLeft">
           <h3 className="courseTitle">{this.props.title}</h3>
           <p className="schoolTitle">{this.props.school}</p>
-          <p className="startDate"> Start Date: November 30 </p>
+          <p className="startDate"> Start: November 30 </p>
           <p className="time">9:30 AM - 10:30 AM PST</p>
           <p className="daysOfWeek"> M, W </p>
         </div>
         <div className="listingRight">
-          <p className="schoolAddress"> 550 NW Alder <br/> Portland, Oregon 97209</p>
-          <Button className="register">Register</Button>
+          <p className="schoolStreet"> 550 NW Alder </p>
+          <p className="schoolCity">Portland, Oregon 97209</p>
+          <Button className="registerButton">Register</Button>
         </div>
       </ListGroupItem>
     );    
