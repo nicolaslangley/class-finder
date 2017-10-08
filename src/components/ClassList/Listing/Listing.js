9168891b75ab4e01d96032e4104e1d5200be2451
Alexis <agregers@uw.edu>
2017-10-07 20:57:17 -0700

    update the panel style

--- src/components/ClassList/Listing/Listing.js
+++ src/components/ClassList/Listing/Listing.js
@@ -5,18 +5,18 @@ import './Listing.css';
 class Listing extends Component {
   render() {
     return (
-      <Panel className="listing" collapsible header={this.props.title} eventKey="1">
-        <div className="listingLeft">
-          <h3 className="courseTitle">{this.props.title}</h3>
-          <p className="schoolTitle">{this.props.school}</p>
-          <p className="startDate"> Start: November 30 </p>
-          <p className="time">9:30 AM - 10:30 AM PST</p>
-          <p className="daysOfWeek"> M, W </p>
-        </div>
-        <div className="listingRight">
-          <p className="schoolStreet"> 550 NW Alder </p>
-          <p className="schoolCity">Portland, Oregon 97209</p>
-          <Button className="registerButton">Register</Button>
+      <Panel collapsible header={this.props.title} eventKey="1">
+        <div className="listing">
+          <div className="listingLeft">
+            <p className="schoolName">{this.props.school}</p>
+            <p className="schoolStreet"> 550 NW Alder </p>
+            <p className="schoolCity">Portland, Oregon 97209</p>
+          </div>
+          <div className="listingRight">
+            <p className="startDate"> Start: November 30 </p>
+            <p className="daysTime">M, W 9:30 AM - 10:30 AM PST</p>
+            <Button className="registerButton">Register</Button>
+          </div>
         </div>
       </Panel>
     );    
